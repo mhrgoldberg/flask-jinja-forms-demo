ff55dbf814f1e664480570a30639ed909d40f8be
mitchellreiss <mitchellreiss@gmail.com>
2021-06-08 08:12:33 -0700

    Update: 🧩 Simplify routes for 2-blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask
 from app.config import Config
-from .routes import workout_routes, workouts
+from .routes import workout_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # print(app.config['SECRET_KEY'])
+
+
 @app.route('/')
 def index():
-	"""
-	A simple route to display all workouts
-	"""
-	workouts_list = [workout for workout in workouts.values()]
-	return render_template('index.html', workouts=workouts_list)
+    return "Go to /workouts to use the app!"
 
 
 app.register_blueprint(workout_routes)
--- app/routes/workouts.py
+++ app/routes/workouts.py
@@ -1,56 +1,31 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template
 
 
 workout_routes = Blueprint('workouts', __name__, url_prefix='/workouts')
 
 workouts = {
-	1: {
-		'id': 1,
-		'title': 'Mountain Run',
-		'duration': 90,
-		'exertion_level': 'Moderate',
-	}
+    1: {
+        'id': 1,
+        'title': 'Mountain Run',
+        'duration': 90,
+        'exertion_level': 'Moderate',
+    }
 }
 
+
+@workout_routes.route('/')
+def index():
+    """
+    A simple route to display all workouts
+    """
+    workouts_list = [workout for workout in workouts.values()]
+    return render_template('index.html', workouts=workouts_list)
 
 
 @workout_routes.route('/<int:id>')
 def get_single_workout(id):
-	"""
-	Shows a single Form
-	"""
-	print([workouts[id]])
-	return render_template('show.html', workout=workouts[id] )
-
-
-@workout_routes.route('/new')
-def get_workout_form():
-	"""
-	Serves up a new_workout form template
-	"""
-	return render_template('new.html')
-	
-
-@workout_routes.route('/new', methods=['POST'])
-def add_new_workout():
-	"""
-	Validates form data and adds to DB
-	"""
-	form_data = request.form
-
-	if (
-		form_data['title'] and 
-		form_data['duration'] and 
-		form_data['exertion_level']
-		):
-		i = len(workouts) + 1
-		new_workout = {
-			'id': i,
-			'title': form_data['title'],
-			'duration': int(form_data['duration']),
-			'exertion_level': form_data['exertion_level']
-		}
-		workouts[i] = new_workout
-		print(workouts)
-		return redirect(f'/workouts/{i}')
-	return "There was a error submiting your form. Please try again!"
+    """
+    Shows a single Form
+    """
+    print([workouts[id]])
+    return render_template('show.html', workout=workouts[id])
